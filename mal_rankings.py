3720307510682dd2b2253628f3ad736f7b43cca5
Manitary <pak.ska@gmail.com>
2023-01-16 22:32:26 +0000

    Rank calculations from local data

--- mal_rankings.py
+++ mal_rankings.py
@@ -1,40 +1,48 @@
 """Estimate the parameters of the Bradley-Terry model for a sample of MAL users."""
 
-# from datetime import datetime
+import glob
 from numpy import np
 from tqdm import tqdm
 from utils import (
-    collect_sample,
     create_table,
     setup_bradley_terry,
     iterate_parameter,
+    load_id_to_order_map,
+    load_samples,
     TIMESTAMP,
 )
 
-# TIMESTAMP = datetime.now().strftime("%Y%m%d-%H%M%S")
-SAMPLE_SIZE = 10000
 NUM_ITERATIONS = 50
+SAMPLE_PATH = "data/sample_*.json"
 
 
 def main(
-    sample_size: int = SAMPLE_SIZE,
+    sample_path: str = SAMPLE_PATH,
     num_iter: int = NUM_ITERATIONS,
     save: bool = True,
+    timestamp: str = TIMESTAMP,
 ) -> None:
     """Do the entire calculation from scratch."""
-    sample = collect_sample(size=sample_size, save=save)
-    table = create_table(sample=sample, save=save)
+    sample = load_samples(glob.glob(sample_path))
+    id_to_order = load_id_to_order_map()
+    table = create_table(
+        size=len(id_to_order), id_to_order=id_to_order, sample=sample, save=save
+    )
     p, mt, w = setup_bradley_terry(table)
+    # Force initial table deletion if necessary
+    # import gc
+    # del table
+    # gc.collect()
     p_list = [p]
     for _ in tqdm(range(num_iter)):
         p = iterate_parameter(p=p, mt=mt, w=w)
         p_list.append(p)
 
     if save:
-        with open(file=f"data/parameter_{sample_size}_{TIMESTAMP}.npy", mode="wb") as f:
+        with open(file=f"data/parameter_{timestamp}_{len(sample)}.npy", mode="wb") as f:
             np.save(f, p)
         with open(
-            file=f"data/parameters_{sample_size}_{TIMESTAMP}.npz", mode="wb"
+            file=f"data/parameters_{timestamp}_{len(sample)}.npz", mode="wb"
         ) as f:
             np.savez(f, *p_list)
     print("Finished")
