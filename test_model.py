e9d4211a679b6f03f8f39f16e3bde74d9890314f
Manitary <pak.ska@gmail.com>
2023-01-17 14:51:04 +0000

    Add A > B = C > D test case

--- test_model.py
+++ test_model.py
@@ -7,8 +7,21 @@ from utils import setup_bradley_terry, iterate_parameter, create_table
 
 @fixture(name="table_one_user_three_anime")
 def fixture_table_one_user_three_anime() -> np.ndarray:
-    """Small model table."""
+    """Small model table.
+
+    Return the table for a single user voting A > B > C."""
     table = np.array([[0, 1, 1], [0, 0, 1], [0, 0, 0]], dtype=int)
+    yield table
+
+
+@fixture(name="table_two_users_four_anime")
+def fixture_table_two_users_four_anime() -> np.ndarray:
+    """Small non-trivial model table.
+
+    Return the table for two users voting:
+        A > B > C > D
+        A > C > B > D"""
+    table = np.array([[0, 2, 2, 2], [0, 0, 1, 2], [0, 1, 0, 2], [0, 0, 0, 0]])
     yield table
 
 
@@ -58,6 +71,14 @@ def test_one_user_three_anime(table_one_user_three_anime: np.ndarray) -> None:
     p, mt, w = setup_bradley_terry(table_one_user_three_anime)
     p = iterate_parameter(p=p, mt=mt, w=w)
     assert p[0] > p[1] > p[2]
+
+
+def test_two_users_four_anime(table_two_users_four_anime: np.ndarray) -> None:
+    """Test a simple case: 2 users, 4 anime ranked."""
+    p, mt, w = setup_bradley_terry(table_two_users_four_anime)
+    for _ in range(5):
+        p = iterate_parameter(p=p, mt=mt, w=w)
+        assert p[0] > p[1] == p[2] > p[3]
 
 
 def test_create_table_one_user(
