Fix error calculation again

mal_rankings.py
@@ -69,7 +69,7 @@ def endless_iteration(
         with open(f"data/{timestamp}_{sample_size}/last_delta_{marker}.npy", "wb") as f:
             np.save(f, last_delta)
         with open(f"data/{timestamp}_{sample_size}/error_pct_{marker}.npy", "wb") as f:
-            np.save(f, np.abs(last_delta - p) / p * 100)
+            np.save(f, last_delta / p * 100)
 
 
 def initialise(