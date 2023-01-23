0c335f600c378f867822d899fece6441b0826646
Manitary <pak.ska@gmail.com>
2023-01-23 00:46:24 +0000

    Update webpage
    Change font
    Change link colour
    Display - instead of 0% in case of no error in the parameter
    Add navigation menu at the top
    Various rephrasing and formatting

--- scripts.js
+++ scripts.js
@@ -56,13 +56,13 @@ function fillTable(elementID, dataSelectorID, cutoffSelectorID) {
             var diff = anime[id]['rank'] - i
             tr.innerHTML = '<td>' + i + '</td>' +
                 '<td>' + anime[id]['rank'] + '</td>' +
-                '<td>' + (diff<=0?"":"+") + diff + '</td>' +
+                '<td>' + (diff <= 0 ? "" : "+") + diff + '</td>' +
                 '<td>' + anime[id]['popularity'] + '</td>' +
                 '<td>' + (anime[id]['score'] || '-') + '</td>' +
                 '<td>' + entry['num_comparisons'] + '</td>' +
                 '<td>' + entry['num_lists'] + '<br />' + '(' + entry['pct_lists'].toFixed(2) + '%)' + '</td>' +
                 '<td>' + entry['parameter'] + '</td>' +
-                '<td>' + entry['rel_error_pct'].toFixed(2) + '%' + '</td>' +
+                '<td>' + (entry['rel_error_pct'] == 0 ? '-' : entry['rel_error_pct'].toFixed(2) + '%') + '</td>' +
                 '<td>' + anime[id]['title'] + '<br />' + (anime[id]['title_en'] || '-') + '</td>';
             table.appendChild(tr);
         };
